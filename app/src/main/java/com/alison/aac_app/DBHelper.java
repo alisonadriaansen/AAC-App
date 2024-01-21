428f1350607f9691130cd3355991a0aca798f593
Alison Adriaansen <alison.adriaansen@mycit.ie>
2024-01-21 20:43:31 +0000

    Reads db file and prints 'words' values to screen

---
+++ app/src/main/java/com/alison/aac_app/DBHelper.java
@@ -0,0 +1,110 @@
+package com.alison.aac_app;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private final static String TAG = "DatabaseHelper";
+    private final Context myContext;
+    private static final String DATABASE_NAME = "aacDB.sqlite3";
+    private static final int DATABASE_VERSION = 1;
+    private final String pathToSaveDBFile;
+    public DBHelper(Context context, String filePath) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.myContext = context;
+        pathToSaveDBFile = filePath + "/" + DATABASE_NAME;
+    }
+
+    public void prepareDatabase() throws IOException {
+        boolean dbExist = checkDataBase();
+        if(dbExist) {
+            Log.d(TAG, "Database exists.");
+            int currentDBVersion = getVersionId();
+            if (DATABASE_VERSION > currentDBVersion) {
+                Log.d(TAG, "Database version is higher than old.");
+                deleteDb();
+                try {
+                    copyDataBase();
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        } else {
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+    }
+    private boolean checkDataBase() {
+        boolean checkDB = false;
+        try {
+            File file = new File(pathToSaveDBFile);
+            checkDB = file.exists();
+        } catch(SQLiteException e) {
+            Log.d(TAG, e.getMessage());
+        }
+        return checkDB;
+    }
+    private void copyDataBase() throws IOException {
+        OutputStream os = new FileOutputStream(pathToSaveDBFile);
+        InputStream is = myContext.getAssets().open("databases/"+DATABASE_NAME);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = is.read(buffer)) > 0) {
+            os.write(buffer, 0, length);
+        }
+        is.close();
+        os.flush();
+        os.close();
+    }
+    public void deleteDb() {
+        File file = new File(pathToSaveDBFile);
+        if(file.exists()) {
+            file.delete();
+            Log.d(TAG, "Database deleted.");
+        }
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(TAG, "onCreate");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+    public List<String> getWords() {
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(pathToSaveDBFile, null, SQLiteDatabase.OPEN_READONLY);
+        String query = "SELECT * FROM words";
+        Cursor cursor = db.rawQuery(query, null);
+        List<String> list = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            String word;
+            word = cursor.getString(0);
+            list.add(word);
+        }
+        db.close();
+        return list;
+    }
+    private int getVersionId() {
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(pathToSaveDBFile, null, SQLiteDatabase.OPEN_READONLY);
+        String query = "SELECT version_id FROM dbVersion";
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+        int v =  cursor.getInt(0);
+        db.close();
+        return v;
+    }
+}
--- app/src/main/java/com/alison/aac_app/MainActivity.java
+++ app/src/main/java/com/alison/aac_app/MainActivity.java
@@ -3,12 +3,31 @@ package com.alison.aac_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final static String TAG = "MainActivity";
+    DBHelper dbHelper= null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper = new DBHelper(this, getFilesDir().getAbsolutePath());
+        try {
+            dbHelper.prepareDatabase();
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+        }
+        showData();
+    }
+
+    private void showData() {
+        List<String> list = dbHelper.getWords();
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(list.toString());
     }
 }
